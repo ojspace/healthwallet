5f4559c2a1dbcafb254bc1475b41e7d3b6e569d6
themanfromspace <o.jafarli@gmail.com>
2026-01-24 13:57:28 +0400

    Initial Commit

---
+++ Healthwallet/HealthwalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthwalletApp.swift
+//  Healthwallet
+//
+//  Created by oj on 24.01.26.
+//
+
+import SwiftUI
+
+@main
+struct HealthwalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthwalletTests/HealthwalletTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthwalletTests.swift
+//  HealthwalletTests
+//
+//  Created by oj on 24.01.26.
+//
+
+import Testing
+@testable import Healthwallet
+
+struct HealthwalletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
